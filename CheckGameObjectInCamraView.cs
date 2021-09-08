e78cce7f137e77a84e0cd5d6ca3ff7da7b6c3edc
GdRLT <65898512+GdRLT@users.noreply.github.com>
2021-09-08 12:36:48 +0300

    Add files via upload

---
+++ CheckGameObjectInCamraView.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckGameObjectInCamraView : MonoBehaviour
+{
+    public GameObject target;
+
+    void Update()
+    {
+        Vector3 screenPoint = Camera.main.WorldToViewportPoint(target.transform.position);
+
+        bool onScreen = screenPoint.z > 0 && screenPoint.x > 0 && screenPoint.x < 1 && screenPoint.y > 0 && screenPoint.y < 1;
+
+        if (onScreen)
+            print("target inside of the Screen");
+        else
+            print("target outside of the Screen");
+    }
+}
