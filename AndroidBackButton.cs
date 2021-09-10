f72020fd743d7a5ac8fadc56f3a37cbb41a7af0b
GdRLT <65898512+GdRLT@users.noreply.github.com>
2021-09-10 10:03:43 +0300

    Android Back or Cancel Button
    This code snippet lets you use Return(Back) button on Android Devices

---
+++ AndroidBackButton.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private void AndroidBackOrEscapeButton()
+    {
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                //Do Stuffs ...
+            }
+        }
+    }
+}
