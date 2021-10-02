81cbdf7d544226033442bd6909e432efc73f0af5
GdRLT <65898512+GdRLT@users.noreply.github.com>
2021-10-02 20:36:42 +0300

    Add files via upload

---
+++ StopAsyncAfterExitPlayMode.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Reflection;
+using System.Threading;
+using UnityEngine;
+
+public class StopAsyncAfterExitPlayMode : MonoBehaviour
+{
+    void OnApplicationQuit()
+    {
+    #if UNITY_EDITOR
+        var constructor = SynchronizationContext.Current.GetType().GetConstructor(BindingFlags.NonPublic | BindingFlags.Instance, null, new Type[] { typeof(int) }, null);
+        var newContext = constructor.Invoke(new object[] { Thread.CurrentThread.ManagedThreadId });
+        SynchronizationContext.SetSynchronizationContext(newContext as SynchronizationContext);
+    #endif
+    }
+}
