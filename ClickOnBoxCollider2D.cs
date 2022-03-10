679082678d44d38d2dd40965f6b0bfb63121da5f
GdRLT <65898512+GdRLT@users.noreply.github.com>
2022-03-10 13:53:24 +0300

    Create ClickOnBoxCollider2D.cs

---
+++ ClickOnBoxCollider2D.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class ClickOnBoxCollider2D : MonoBehaviour
+{
+void Update()
+{
+    if (Input.GetMouseButtonDown(0))
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, Mathf.Infinity);
+        if (hit)
+        {
+            Debug.Log(hit.collider.gameObject.name);
+}}}}
